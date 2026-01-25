54e719813270580052310d53baf26a9c9778c06b
LawrenceVelilla <velilla.lawrencephc@gmail.com>
2026-01-25 01:02:47 -0700

    added two more urls for the cors config

--- backend/app.ts
+++ backend/app.ts
@@ -15,7 +15,9 @@ const allowedOrigins = [
     'http://localhost:5173',
     'http://localhost:3000',
     process.env.FRONTEND_URL1,
-    process.env.FRONTEND_URL2
+    process.env.FRONTEND_URL2,
+    process.env.FRONTEND_URL3,
+    process.env.FRONTEND_URL4,
 ].filter(Boolean);
 
 app.use(cors({
